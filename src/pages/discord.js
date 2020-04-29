ec16310e1486d243b19d000df8686a760e214a0c
Hammad Jutt <jutt@ualberta.ca>
2020-04-29 12:19:04 -0600

    Add redirect to Discord
    This adds a redirect to SourceCred's Discord server when the user navigates to sourcecred.io/discord
    
    Test Plan: Ensure the URL correctly redirects to the discord invite

---
+++ src/pages/discord.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Discord = () => {
+  React.useEffect(() => {
+    window.location.replace('https://discord.gg/SXreMyQ')
+  }, [])
+  
+  return null;
+};
+
+export default Discord;
