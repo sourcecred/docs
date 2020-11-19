28d4a64c3ac1d5eb5989061d846ff2d770ed8354
Faruk Ateş <faruk@goinglovefirst.com>
2020-11-19 11:17:24 -0800

    Update website footer labels (#245)
    "Chat" "Forum" and "Our Cred" were not very clear labels to newcomers, since we talk predominantly about our Discord, our Discourse, and our Cred Explorer.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,19 +28,19 @@ module.exports = {
     footer: {
       links: [
         {
-          title: "Community",
+          title: "Join our Community",
           items: [
             {
-              label: "Chat",
+              label: "Discord Chat",
               href: "https://sourcecred.io/discord",
             },
             {
-              label: "Forums",
+              label: "Discourse Forums",
               href: "https://discourse.sourcecred.io",
             },
             {
-              label: "Our Cred",
-              to: "http://cred.sourcecred.io/#/",
+              label: "Our Cred Explorer",
+              to: "http://cred.sourcecred.io/",
             },
           ],
         },
